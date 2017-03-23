d1d397b3a492101e4606dea723e454e589f6ced6
Michail Pantourakis <padoura@users.noreply.github.com>
2017-03-24 00:07:57 +0200

    Small corrections and comments
    - Added a missing (double) for the calculation of maxPage in FileHandler
    which caused the last page to be skipped instead of searched
    - FileHandler.create method now fills any possible empty slots of the
    last disk page with +Inf to ensure that Arrays.binarySearch will work
    correctly on it
    - Added GNU general public license message
    - Added/corrected a few comments

--- COMP211_exercise1/src/gr/ece/tuc/comp211/FileHandler.java
+++ COMP211_exercise1/src/gr/ece/tuc/comp211/FileHandler.java
@@ -1,3 +1,22 @@
+/*
+* This code is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License version as
+* published by the Free Software Foundation, either version 3 of the License, 
+* or (at your option) any later version.
+*
+* This code is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+* version 3 for more details.
+*
+* You should have received a copy of the GNU General Public License version
+* 3 along with this work; if not, see <http://www.gnu.org/licenses/>.
+*
+* Please contact Michail Pantourakis via Github repository 
+* https://github.com/padoura/gr.ece.tuc.comp211 if you need additional 
+* information or have any questions.
+*/
+
 package gr.ece.tuc.comp211;
 
 import java.io.FileNotFoundException;
@@ -15,17 +34,16 @@ public class FileHandler extends RandomAccessFile{
 
 	public FileHandler(String name, String mode) throws FileNotFoundException {
 		super(name, mode);
-		// TODO Auto-generated constructor stub
 	}
 
 	// ---------------The 4 Search methods----------------------------------------
 	public double searchSequential(int amountOfSearches, int bufferSize, int numberOfInt) throws IOException{
 		double diskAccesses = 0;
-		long pagePointer;
+		long filePointer;
 		int [] randomInteger = this.getRandomIntegers(amountOfSearches, numberOfInt); // get an array of random integers
 		DiskPage diskPage;
 		int currentPage;
-		int maxPage = (int) Math.ceil(numberOfInt*Integer.BYTES/bufferSize); // total number of diskpages
+		int maxPage = (int) Math.ceil((double) numberOfInt*Integer.BYTES/bufferSize); // total number of diskpages
 		int indexFound;
 		
 //		System.err.println("---------------------------------");
@@ -38,9 +56,9 @@ public class FileHandler extends RandomAccessFile{
 		
 			do{
 				currentPage++;
-				pagePointer = (currentPage-1)*bufferSize;
-				diskPage = this.readPage(bufferSize, pagePointer);
-				pagePointer += bufferSize;
+				filePointer = (currentPage-1)*bufferSize;
+				diskPage = this.readPage(bufferSize, filePointer);
+				filePointer += bufferSize;
 				diskAccesses++;
 				indexFound = Arrays.binarySearch(diskPage.intBuffer, randomInteger[searchCount]); // negative if not found
 			} while ( currentPage < maxPage &&  indexFound < 0);
@@ -53,9 +71,8 @@ public class FileHandler extends RandomAccessFile{
 	
 
 	public double searchBinary(int amountOfSearches, int bufferSize, int numberOfInt) throws IOException {
-		// TODO Auto-generated method stub
 		double diskAccesses = 0;
-		long pagePointer;
+		long filePointer;
 		int maxPage;
 		int minPage;
 		int currentPage;
@@ -69,16 +86,17 @@ public class FileHandler extends RandomAccessFile{
 
 		
 		for (int searchCount = 0; searchCount < amountOfSearches; searchCount++){
-			maxPage = (int) Math.ceil(numberOfInt*Integer.BYTES/bufferSize);
+			maxPage = (int) Math.ceil((double) numberOfInt*Integer.BYTES/bufferSize);
 			minPage = 1;
 
 			
 			do {
 				currentPage = (maxPage + minPage)/2 ; // get the middle page
-				pagePointer = (long) ((currentPage-1)*bufferSize); // start from the middle page of the file
-				diskPage = this.readPage(bufferSize, pagePointer);
+				filePointer = (long) ((currentPage-1)*bufferSize); // start from the middle page of the file
+				diskPage = this.readPage(bufferSize, filePointer);
 				diskAccesses++;
-				indexFound = Arrays.binarySearch(diskPage.intBuffer, randomInteger[searchCount]); // -1 if smaller, -length-1 if larger
+				// indexFound = -1 if smaller, -length-1 if larger
+				indexFound = Arrays.binarySearch(diskPage.intBuffer, randomInteger[searchCount]); 
 				
 				if ( indexFound != -1)
 					minPage = currentPage+1; // go to the right side
@@ -97,8 +115,8 @@ public class FileHandler extends RandomAccessFile{
 
 	public double searchOrderedQueries(int amountOfSearches, int bufferSize, int numberOfInt) throws IOException {
 		double diskAccesses = 0;
-		long pagePointer;
-		int maxPage = (int) Math.ceil(numberOfInt*Integer.BYTES/bufferSize);
+		long filePointer;
+		int maxPage = (int) Math.ceil((double) numberOfInt*Integer.BYTES/bufferSize);
 		int minPage = 1;
 		int currentPage;
 		int [] randomInteger = this.getRandomIntegers(amountOfSearches, numberOfInt); // get an array of random integers
@@ -106,11 +124,11 @@ public class FileHandler extends RandomAccessFile{
 		int indexFound;
 		
 		Arrays.sort(randomInteger); // sort the wanted integers before searching
-		
+		// run it once to get the first page
 		do {
 			currentPage = (maxPage + minPage)/2 ; // get the middle page
-			pagePointer = (long) ((currentPage-1)*bufferSize); // start from the middle page of the file
-			diskPage = this.readPage(bufferSize, pagePointer);
+			filePointer = (long) ((currentPage-1)*bufferSize); // start from the middle page of the file
+			diskPage = this.readPage(bufferSize, filePointer);
 			diskAccesses++;
 			indexFound = Arrays.binarySearch(diskPage.intBuffer, randomInteger[0]); // -1 if smaller, -length-1 if larger
 			
@@ -127,15 +145,15 @@ public class FileHandler extends RandomAccessFile{
 		
 		
 		for (int searchCount = 1; searchCount < amountOfSearches; searchCount++){
-			maxPage = (int) Math.ceil(numberOfInt*Integer.BYTES/bufferSize);
+			maxPage = (int) Math.ceil((double) numberOfInt*Integer.BYTES/bufferSize);
 			minPage = 1;
 			
 			
 			indexFound = Arrays.binarySearch(diskPage.intBuffer, randomInteger[searchCount]); // check whether next integer is in the current buffer
 			while ( indexFound < 0 &&  maxPage >= minPage  ){ // If not in memory, search the file again
 				currentPage = (maxPage + minPage)/2 ; // get the middle page
-				pagePointer = (long) ((currentPage-1)*bufferSize); // start from the middle page of the file
-				diskPage = this.readPage(bufferSize, pagePointer);
+				filePointer = (long) ((currentPage-1)*bufferSize); // start from the middle page of the file
+				diskPage = this.readPage(bufferSize, filePointer);
 				diskAccesses++;
 				indexFound = Arrays.binarySearch(diskPage.intBuffer, randomInteger[searchCount]);
 				if ( indexFound != -1)
@@ -146,7 +164,7 @@ public class FileHandler extends RandomAccessFile{
 			
 //			System.err.println("Number " + randomInteger[searchCount] + " found in page " + currentPage + " and position " + indexFound);
 
-		}		
+		}
 		
 		return diskAccesses/amountOfSearches;
 	}
@@ -157,8 +175,8 @@ public class FileHandler extends RandomAccessFile{
 
 	public double searchMemoryQueue(int amountOfSearches, int bufferSize, int numberOfInt, int queueSize) throws IOException {
 		double diskAccesses = 0;
-		long pagePointer;
-		int maxPage = (int) Math.ceil(numberOfInt*Integer.BYTES/bufferSize);
+		long filePointer;
+		int maxPage = (int) Math.ceil((double) numberOfInt*Integer.BYTES/bufferSize);
 		int minPage = 1;
 		int currentPage;
 		int [] randomInteger = this.getRandomIntegers(amountOfSearches, numberOfInt); // get an array of random integers
@@ -167,11 +185,11 @@ public class FileHandler extends RandomAccessFile{
 		
 		ArrayQueue queue = new ArrayQueue(queueSize);
 		
-		
+		// run it once to fill the queue
 		do {
 			currentPage = (maxPage + minPage)/2 ; // get the middle page
-			pagePointer = (long) ((currentPage-1)*bufferSize); // start from the middle page of the file
-			diskPage = this.readPage(bufferSize, pagePointer);
+			filePointer = (long) ((currentPage-1)*bufferSize); // start from the middle page of the file
+			diskPage = this.readPage(bufferSize, filePointer);
 			diskAccesses++;
 			indexFound = Arrays.binarySearch(diskPage.intBuffer, randomInteger[0]); // -1 if smaller, -length-1 if larger
 			
@@ -191,7 +209,7 @@ public class FileHandler extends RandomAccessFile{
 		
 		
 		for (int searchCount = 1; searchCount < amountOfSearches; searchCount++){
-			maxPage = (int) Math.ceil(numberOfInt*Integer.BYTES/bufferSize);
+			maxPage = (int) Math.ceil((double) numberOfInt*Integer.BYTES/bufferSize);
 			minPage = 1;
 			
 			indexFound = -1;
@@ -202,8 +220,8 @@ public class FileHandler extends RandomAccessFile{
 				if (diskPage.pageNumber == currentPage)
 					indexFound = Arrays.binarySearch(diskPage.intBuffer, randomInteger[searchCount]);
 				else{ // If not in memory, access the file again
-					pagePointer = (long) ((currentPage-1)*bufferSize);
-					diskPage = this.readPage(bufferSize, pagePointer);
+					filePointer = (long) ((currentPage-1)*bufferSize);
+					diskPage = this.readPage(bufferSize, filePointer);
 					diskAccesses++;
 					indexFound = Arrays.binarySearch(diskPage.intBuffer, randomInteger[searchCount]);
 					queue.enqueue(diskPage); // Add last accessed disk page to the memory queue (if queue is full, also delete the oldest disk page)
@@ -226,7 +244,7 @@ public class FileHandler extends RandomAccessFile{
 	
 	
 	// --------------- Utility methods----------------------------------------
-	public int[] getRandomIntegers(int amountOfSearches, int numberOfInt){ // returns an array of random integers
+	public int[] getRandomIntegers(int amountOfSearches, int numberOfInt){ // return an array of random integers
 		Random RNG = new Random();
 		int [] randomInteger = new int[amountOfSearches];
 		for (int i=0; i<amountOfSearches;i++)
@@ -235,46 +253,53 @@ public class FileHandler extends RandomAccessFile{
 	}
 	
 	
-	public DiskPage readPage(int bufferSize, long pagePointer) throws IOException{
-		ByteBuffer byteBuffer = ByteBuffer.allocate(bufferSize);        
+	public DiskPage readPage(int bufferSize, long filePointer) throws IOException{
+		ByteBuffer byteBuffer = ByteBuffer.allocate(bufferSize);      
         IntBuffer intBuffer = byteBuffer.asIntBuffer();
         byte[] buffer = new byte[bufferSize];
         DiskPage diskPage = new DiskPage(bufferSize/Integer.BYTES);
-        this.seek(pagePointer);
+        this.seek(filePointer);
 		this.read(buffer);
 		byteBuffer.put(buffer);
 		intBuffer.get(diskPage.intBuffer);
-		diskPage.pageNumber = (int) ((pagePointer/bufferSize) + 1);
+		diskPage.pageNumber = (int) ((filePointer/bufferSize) + 1);
 		
 		return diskPage;
 		
 	}
 	
 	
-	public void create(int bufferSize, int numberOfInt) throws IOException{ // create a file of integers
+	public void create(int bufferSize, int numberOfInt) throws IOException{ // create a file of integers [1:numberOfInt]
 		
-		ByteBuffer byteBuffer = ByteBuffer.allocate(bufferSize);        
+		ByteBuffer byteBuffer = ByteBuffer.allocate(bufferSize);       
         IntBuffer intBuffer = byteBuffer.asIntBuffer();
-        int[] allIntegers = new int[numberOfInt];
+        
+        int[] allIntegers = new int[numberOfInt + (bufferSize/Integer.BYTES) - numberOfInt % (bufferSize/Integer.BYTES)];
         int counterFile;
         this.seek(0);
+        byte[] buffer;
         
         for (int i=1; i <= numberOfInt; i++)
 			allIntegers[i-1] = i;
+        // ensure that Arrays.binarySearch will work for the last disk page even if it's not completely filled
+        // filling with 0s would not keep that array ordered anymore
+        Arrays.fill(allIntegers, numberOfInt, allIntegers.length, Integer.MAX_VALUE);
         
 		for (counterFile = 1 ; counterFile <= numberOfInt ; counterFile += bufferSize/Integer.BYTES){
 			intBuffer.put(Arrays.copyOfRange(allIntegers, counterFile-1, counterFile-1+bufferSize/Integer.BYTES));
-			byte[] buffer = byteBuffer.array();
+			buffer = byteBuffer.array();
 			this.write(buffer);
 			intBuffer.clear();
 		}
+		
 	}
 	
 	
 	public DiskPage searchInQueue(ArrayQueue queue, int currentPage){
+		//Perform sequential search.
+		//A hash table for constant time search could have used instead.
 		
 		DiskPage diskPage = new DiskPage();
-		
 		
 		do{
 			try {
--- COMP211_exercise1/src/gr/ece/tuc/comp211/MainController.java
+++ COMP211_exercise1/src/gr/ece/tuc/comp211/MainController.java
@@ -1,24 +1,66 @@
 package gr.ece.tuc.comp211;
 
+
+/*
+* This code is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License version as
+* published by the Free Software Foundation, either version 3 of the License, 
+* or (at your option) any later version.
+*
+* This code is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+* version 3 for more details.
+*
+* You should have received a copy of the GNU General Public License version
+* 3 along with this work; if not, see <http://www.gnu.org/licenses/>.
+*
+* Please contact Michail Pantourakis via Github repository 
+* https://github.com/padoura/gr.ece.tuc.comp211 if you need additional 
+* information or have any questions.
+*/
+
+
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+
+/**
+* This class consists the main launching file of Exercise 1 of 
+* the course "Data Structures and Files", Spring Semester 2017, ECE, 
+* Technical University of Crete. This is an introductory exercise to basic 
+* searching methods on disks and their complexity. 10^7 integers, 
+* ordered from 1 to 10^7, are stored in a file, assuming a page size of 512 bytes. 
+* This file is subsequently used to compare their average disk accesses of four 
+* methods that search for 10000 random integers: 
+* 1) sequential search (first come, first served basis), 
+* 2) binary search (first come, first served basis), 
+* 3) binary search on sorted and batch examined keys, 
+* 4) binary search with "page-caching" using a queue.
+*
+*
+* @author	Michail Pantourakis
+* @see		FileHandler
+* @since	1.8
+*/
+
 
 public class MainController {
 	
 	
 
 	public static void main(String[] args) throws IOException{
-		int bufferSize = 512; // Give the buffer size here
-		int numberOfInt = 78125*128; // Give the total number of integers to save in file, ordered from 1 to numberOfInt. 78125*128 = 10^7 integers, 78125 disk pages
-		String filename = "testfile.bin"; // Give the filename here
+		int bufferSize = Integer.BYTES*128; // Give the buffer size as a number of integers here
+		int numberOfInt = 78125*128; // Give the total number of integers to save in file. 78125*128 = 10^7 integers, 78125 disk pages
+		String filename = "testfile.bin"; // Give the filename here. This file is DELETED in the end to ensure a new file!
 		int numberOfQueries = 10000; // Give how many integers will be searched
 		int[] queueSize = {1, 50, 100}; // Give the size of the queue in main memory (number of buffers)
 		
-		
-		PrintStream log = new PrintStream(new FileOutputStream("logfile.txt"));
-		System.setOut(log); // Log file where every searched integer and its position is printed.
+//		// Uncomment here and in FileHandler if there is a need to check where each number if found
+//		PrintStream log = new PrintStream(new FileOutputStream("logfile1.txt"));
+//		System.setErr(log); // Log file where every searched integer and its position is printed. 
 
 		
 		
@@ -32,7 +74,7 @@ public class MainController {
 
 		System.out.println("Counting average number of disk accesses...");
 		// Searches random integers in a file by accessing diskpages sequentially (Question A)
-		System.out.println("Sequential search: " + file.searchSequential(numberOfQueries, bufferSize, numberOfInt));
+//		System.out.println("Sequential search: " + file.searchSequential(numberOfQueries, bufferSize, numberOfInt));
 		// Searches random integers in a file by using Binary search (on diskpages) (Question B)
 		System.out.println("Binary search: " + file.searchBinary(numberOfQueries, bufferSize, numberOfInt));
 		// Searches random integers in a file by using Binary search (on diskpages) and by ordering queries (Question C)
@@ -41,7 +83,7 @@ public class MainController {
 		for (int i=0;i<queueSize.length;i++)
 			System.out.println("Binary search with a queue in memory (size = " + queueSize[i] + "): "  + file.searchMemoryQueue(numberOfQueries, bufferSize, numberOfInt, queueSize[i]));
 		file.close();
-		log.close();
+//		log.close();
 		
 		File oldFile = new File(filename);
 		oldFile.delete();
